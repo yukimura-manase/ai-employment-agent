c47d5027d554f5a25a2696f95a46db17eb7d8b47
tmorikane <rick0320.tm@gmail.com>
2025-02-07 18:27:10 +0900

    fix: input -> request

--- ai-agent/api/routers/job_search.py
+++ ai-agent/api/routers/job_search.py
@@ -1,20 +1,20 @@
 import os
 from fastapi import APIRouter, HTTPException, BackgroundTasks
-from models.job_search import JobSearchInput, JobSearchResponse
+from models.job_search import JobSearchRequest, JobSearchResponse
 from api.services.job_search import SimpleLangChainAgent, JobSearchAgent
 from typing import List
 from api.services import db_service
 
 router = APIRouter(prefix="/job-search", tags=["job-search"])
 
-# JobSearchAgentを実行
+# 仮のJobSearchAgentを実行
 @router.post("/search")
 async def job_search(
-    search_input: JobSearchInput,
+    request: JobSearchRequest,
     background_tasks: BackgroundTasks
 ) -> List[dict]:
     try:
-        search_result_items = JobSearchAgent(search_input).execute()
+        search_result_items = JobSearchAgent(request).execute()
 
         # バックグラウンドでDB保存処理を実行
         background_tasks.add_task(db_service.save_entrysheet_items, search_result_items)
--- ai-agent/models/job_search.py
+++ ai-agent/models/job_search.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from typing import List
 
 # 求人検索の入力クラス
-class JobSearchInput(BaseSchema):
+class JobSearchRequest(BaseSchema):
     user_id: str
     system_prompt: str
     need_properties: dict
